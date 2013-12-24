0fa901473ac8d129d4d788781b1f689d834c5944
duongbq <duongbq@duongbq-PC>
2013-12-24 17:22:37 +0700

    Sua loi khi chua co model nao trong he thong

--- application/views/layouts/home/home_social.php
+++ application/views/layouts/home/home_social.php
@@ -1,9 +1,9 @@
 <ul class="social-icons">
     <li><a class="social_rss" data-original-title="Feed" href="#"></a></li>
     <li><a class="social_facebook" data-original-title="Facebook" href="#"></a></li>
-    <li><a class="social_twitter" data-original-title="Twitter" href="#"></a></li>
-    <li><a class="social_googleplus" data-original-title="Goole Plus" href="#"></a></li>
-    <li><a class="social_youtube" data-original-title="Youtube" href="#"></a></li>
-    <li class="last" ><a class="social_pintrest" data-original-title="Pinterest" href="#"></a></li>
+    <!--<li><a class="social_flickr" data-original-title="Flickr" href="#"></a></li>-->
+    <!--<li><a class="social_googleplus" data-original-title="Goole Plus" href="#"></a></li>-->
+    <!--<li><a class="social_youtube" data-original-title="Youtube" href="#"></a></li>-->
+    <li class="last" ><a class="social_flickr" data-original-title="Flickr" href="#"></a></li>
 </ul>
 
